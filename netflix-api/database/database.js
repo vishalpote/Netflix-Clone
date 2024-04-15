6cbada8f26cb6461a926ad47d2fe13f571f2355b
vishal <vishalpote9090@gmail.com>
2024-04-15 11:20:23 +0530

    Change DB URL

--- netflix-api/database/database.js
+++ netflix-api/database/database.js
@@ -5,9 +5,7 @@ dotenv.config();
 // const password=process.env.PASSWORD;
 module.exports.connection = async () => {
   try {
-    await mongoose.connect(
-      `mongodb+srv://vishal:vishalpote@netflix.hj3atfm.mongodb.net/?retryWrites=true&w=majority`
-    );
+    await mongoose.connect("mongodb+srv://vishal:vishalpote@netflix.hj3atfm.mongodb.net/?retryWrites=true&w=majority");
     console.log("MongoDB connection established..!!");
   } catch (error) {
     console.log("Error While Connecting...!!");

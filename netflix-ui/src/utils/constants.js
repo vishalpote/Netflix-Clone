8dd55df2dac8458387a738c29461971ac8d705b2
vishal <vishalpote9090@gmail.com>
2024-04-26 15:47:23 +0530

    Change Backend Url

--- netflix-ui/src/utils/constants.js
+++ netflix-ui/src/utils/constants.js
@@ -1,6 +1,6 @@
 export const API_KEY = "3d39d6bfe362592e6aa293f01fbcf9b9";
 export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
-export const BACKEND_API = "https://netflix-clone-2-b0g7.onrender.com";
+export const BACKEND_API = "https://netflix-clone-3-lxty.onrender.com/";
 
 export const host='http://localhost:5000/api/user';
 export const signupRoute=`${host}/signup`;
